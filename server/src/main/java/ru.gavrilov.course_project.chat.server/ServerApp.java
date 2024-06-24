fed4c9d0b9107a07dbbec82a1c6cc9b6bbd68175
mctapok <vektor.ct@gmail.com>
2024-06-24 18:58:17 +0300

    рефаткор кода считывание команд вынесено в отдельный метод обработка команд вынесена в отдельные методы доработана логика работы таймеров добавлен логгер добавлен пропертис для бд исправлена работа постоянного бана

--- server/src/main/java/ru.gavrilov.course_project.chat.server/ServerApp.java
+++
@@ -1,9 +0,0 @@
-package ru.gavrilov.course_project.chat.server;
-
-
-public class ServerApp {
-    public static void main(String[] args) {
-        Server server = new Server(8189);
-        server.start();
-    }
-}
